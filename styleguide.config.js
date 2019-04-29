1be52a223d7ebb968144b43852963e00cb94cbd5
sandrine <sandrinezocca@gmail.com>
2019-04-29 20:23:10 +0200

    test

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,5 +4,13 @@ module.exports = {
     pagePerSection: true,
     ribbon: {
         url: 'https://github.com/sandix34/React-OpenLayers' 
-	}
+    },
+    dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+        // WARNING: inspect Styleguidist Webpack config before modifying it, otherwise you may break Styleguidist
+        console.log(webpackConfig)
+        webpackConfig.externals = {
+          jquery: 'jQuery'
+        }
+        return webpackConfig
+      }
 };
---
+++ styleguide/precache-manifest.89e89d0d964fed23f8a4cce10aa7ef8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862de88343530ac977c6",
+    "url": "build/2.862de883.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "8b65fe50e8b576936a3c",
+    "url": "build/main.8b65fe50.js"
+  },
+  {
+    "revision": "b72f2c314a42c9cc71311f5db18e50db",
+    "url": "index.html"
+  },
+  {
+    "revision": "862de88343530ac977c6",
+    "url": "static/css/2.2a99b240.chunk.css"
+  }
+]);

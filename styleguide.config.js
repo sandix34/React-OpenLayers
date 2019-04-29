99fdb2b496ff60c64f6d4cebb7383482b6e4184a
sandrine <sandrinezocca@gmail.com>
2019-04-29 16:58:25 +0200

    add ribbon

---
+++ styleguide.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Collection de composants React-OpenLayers ',
+    exampleMode: 'hide',
+    pagePerSection: true,
+    ribbon: {
+        url: 'https://github.com/sandix34/React-OpenLayers' 
+	}
+};
